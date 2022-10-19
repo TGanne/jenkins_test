21e4f4e21cff00055b3333599a7edd7dbdbdfc69
TGanne <70717769+TGanne@users.noreply.github.com>
2022-10-20 01:19:01 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
 public static void main (String[] args){
- System.out.println("updated auto chef ");
+ System.out.println("updated auto 2 chef ");
    System.out.println("hello chef");
 }
 }
